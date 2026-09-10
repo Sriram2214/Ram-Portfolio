659485379f0131f750521798a02b5300e030c94c
sriram2214 <kit29.am48@gmail.com>
2026-09-10 16:18:21 +0530

    fix unused social profile code

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import {
   Award,
   ExternalLink,
   CheckCircle,
-  ShieldCheck,
+
   Send
 } from "lucide-react";
 
@@ -516,72 +516,7 @@ export default function App() {
     }
   };
 
-  // Uiverse-Inspired 4 Circular Social Profiles Data (Slide 9: Contact)
-  const socialProfilesData: SocialProfile[] = [
-    {
-      id: "linkedin",
-      name: "LinkedIn",
-      url: "https://www.linkedin.com/in/sriram-m-90287537b/",
-      tooltip: "LinkedIn",
-      accentColor: "#0077b5",
-      logoType: "linkedin"
-    },
-    {
-      id: "github",
-      name: "GitHub",
-      url: "https://github.com/Sriram2214",
-      tooltip: "GitHub",
-      accentColor: "#24292e",
-      logoType: "github"
-    },
-    {
-      id: "codechef",
-      name: "CodeChef",
-      url: "https://www.codechef.com/users/srirammuthaiya",
-      tooltip: "CodeChef",
-      accentColor: "#5b4638",
-      logoType: "codechef"
-    },
-    {
-      id: "leetcode",
-      name: "LeetCode",
-      url: "https://leetcode.com/u/SriramMuthaiya/",
-      tooltip: "LeetCode",
-      accentColor: "#ffa116",
-      logoType: "leetcode"
-    }
-  ];
-
-  const renderSocialLogo = (type: string) => {
-    switch (type) {
-      case "linkedin":
-        return (
-          <svg viewBox="0 0 24 24" width="22" height="22" className="social-svg-icon">
-            <path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.46 10.9v8.37H9.25V10.9H6.46M7.86 6.74a1.62 1.62 0 1 0 0 3.24 1.62 1.62 0 0 0 0-3.24z"/>
-          </svg>
-        );
-      case "github":
-        return (
-          <svg viewBox="0 0 24 24" width="22" height="22" className="social-svg-icon">
-            <path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z"/>
-          </svg>
-        );
-      case "codechef":
-        return (
-          <svg viewBox="0 0 24 24" width="22" height="22" className="social-svg-icon">
-            <path fill="currentColor" d="M12 3c-1.38 0-2.5 1.12-2.5 2.5 0 .28.05.54.14.78C8.5 6.07 7.5 7.18 7.5 8.5c0 .64.22 1.23.59 1.7-.85.5-1.44 1.4-1.44 2.46 0 1.57 1.28 2.84 2.85 2.84h5c1.57 0 2.85-1.27 2.85-2.84 0-1.06-.59-1.96-1.44-2.46.37-.47.59-1.06.59-1.7 0-1.32-1-2.43-2.14-2.22.09-.24.14-.5.14-.78C14.5 4.12 13.38 3 12 3zm-3.5 14v2.5c0 .55.45 1 1 1h5c.55 0 1-.45 1-1V17h-7z"/>
-          </svg>
-        );
-      case "leetcode":
-        return (
-          <svg viewBox="0 0 24 24" width="22" height="22" className="social-svg-icon">
-            <path fill="currentColor" d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.17 5.79a1.374 1.374 0 0 0-.416.977c.004.37.152.72.416.977l5.352 5.352a1.374 1.374 0 0 0 1.944 0l.96-.96a1.374 1.374 0 0 0 0-1.944l-4.38-4.382 4.38-4.38a1.374 1.374 0 0 0 0-1.944l-.96-.96A1.374 1.374 0 0 0 13.483 0zm-8.83 8.3a1.374 1.374 0 0 0-.977.416L.438 12.008a1.374 1.374 0 0 0 0 1.944l3.238 3.238a1.374 1.374 0 0 0 1.944 0l.96-.96a1.374 1.374 0 0 0 0-1.944l-2.266-2.266 2.266-2.266a1.374 1.374 0 0 0 0-1.944l-.96-.96a1.374 1.374 0 0 0-.977-.416zm14.137 0a1.374 1.374 0 0 0-.977.416l-.96.96a1.374 1.374 0 0 0 0 1.944l2.266 2.266-2.266 2.266a1.374 1.374 0 0 0 0 1.944l.96.96a1.374 1.374 0 0 0 1.944 0l3.238-3.238a1.374 1.374 0 0 0 0-1.944l-3.238-3.292a1.374 1.374 0 0 0-.967-.416zm-5.464 7.644a1.374 1.374 0 0 0-.961.438l-5.352 5.352a1.374 1.374 0 0 0 0 1.944l.96.96a1.374 1.374 0 0 0 1.944 0l5.352-5.352a1.374 1.374 0 0 0 0-1.944l-.96-.96a1.374 1.374 0 0 0-.983-.438z"/>
-          </svg>
-        );
-      default:
-        return null;
-    }
-  };
+  
 
   const toggleInternship = () => {
     setActiveInternImageIndex((prev) => (prev === 0 ? 1 : 0));
@@ -1856,3 +1791,6 @@ export default function App() {
     </div>
   );
 }
+
+
+
